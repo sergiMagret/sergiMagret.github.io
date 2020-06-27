74af4ea4e3d60196104983a817312c3672e9d895
sergiMagret <u1953662@campus.udg.edu>
2020-06-27 17:01:16 +0200

    Quick fix updating the nav-bar

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
-$(document).ready(function (argument) {
+$(document).ready(function () {	
+	$(window).on('scroll', function() {
+		updateNavBar();
+	});
+});
+
+function updateNavBar() {
 	let cover_height = $("#cover").height();
 	let header_height = $("header").height();
 
-	console.log(cover_height);
-	console.log(header_height);
-	
-	$(window).on('scroll', function() {
-		let scroll_top = $(window).scrollTop();
-		if(scroll_top > (cover_height - header_height)){
-			$("header").css('background-color', 'var(--dark-blue)');
-			$("header").addClass('bottom-shadow');
-		}else{
-			$("header").css('background-color', 'transparent');
-			$("header").removeClass('bottom-shadow');
-		}
-		
-		console.log($(window).scrollTop());
-	});
-	
-	console.log("Hello world!");
-});
+	//console.log(cover_height);
+	//console.log(header_height);
+
+	let scroll_top = $(window).scrollTop();
+	//console.log("scroll:" + scroll_top + ", cover_height - header_height: " + (cover_height - header_height));
+	if(scroll_top > (cover_height - header_height)){
+		$("header").css('background-color', 'var(--dark-blue)');
+		$("header").addClass('bottom-shadow');
+	}else{
+		$("header").css('background-color', 'transparent');
+		$("header").removeClass('bottom-shadow');
+	}
+
+	//console.log($(window).scrollTop());
+}
