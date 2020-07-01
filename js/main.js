0aac3804045865d15ea378a115104bb17dd40d4a
sergiMagret <u1953662@campus.udg.edu>
2020-07-01 23:55:07 +0200

    Update whoami section with basic information

--- js/main.js
+++ js/main.js
@@ -21,6 +21,35 @@ function updateNavBar() {
 	//console.log($(window).scrollTop());
 }
 
+function isBirthday(day, month){
+	return month == 7 && day == 24;
+}
+
+function calculateAge(day, month, year) {
+	let day_birth = 24;
+	let month_birth = 7;
+	let year_birth = 1999;
+	let age = year - year_birth;
+	if(isBirthday(day, month)){
+		return age;
+	}else if(month <= month_birth && day <= day_birth){
+		age--;
+		return age;
+	}
+}
+
 $(document).ready(function() {
 	$(window).on('scroll', updateNavBar);
+
+	let dt = new Date();
+	let age = calculateAge(dt.getUTCDate(), dt.getUTCMonth()+1, dt.getUTCFullYear());
+	
+	// Update the tag in the whoami section to display the correct age without having to update it manually
+	// When it's my birthday is displayed as well.
+	if(isBirthday(dt.getUTCDate(), dt.getUTCMonth()+1)){
+		$("#my-age").html('<i class="fas fa-calendar-day blue-icon"></i>' + "Today is my birthday!! I'm turning " + age);	
+	}else{
+		$("#my-age").html('<i class="fas fa-calendar-day blue-icon"></i>' + age + " years old");
+	}
+	
 });
